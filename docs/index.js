17e7ea340f654d762948dab4434ab027bdbbbce4
Jorge Besnier <jorgebesnierb@outlook.com>
2022-09-18 21:01:12 -0500

    full solution due to problems with github

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,90 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+//Import all the requiere modules
+const inquirer = require('inquirer');
+const questions = require ('./utils/inquirerQA.js')
+const generateMarkdown = require ('./utils/generateMarkdown.js')
+const fs = require ('fs');
+const { Console } = require('console');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`./${fileName}`, data, (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+        } 
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const QuestionsSingle = questions.questionsOneResponse;
+    const QuestionsMulti = questions.questionsMultiResponse;
+    const AddStep = questions.questionsAddStep;
+    let objGeneral = {};
+
+
+    async function callInquirers() {
+        //Ask for general data that does not need to ask again
+        const inq1 = await inquirer.prompt(QuestionsSingle)
+            .then(res0 => {
+                objGeneral = res0;
+                console.log(objGeneral);
+                return objGeneral;
+        });
+
+        //Ask for as many steps there are in the process.
+        let addBool = true;
+        let questionCont = 0
+        while (questionCont < QuestionsMulti.length) { //THIS COULD BE CHANGED TO A NICE FUNCTION, BUT COULDNT BECAUSE SYNC OF CALLS
+            objGeneral[QuestionsMulti[questionCont].name] = [];
+            while(addBool){
+                const inq2 = await inquirer.prompt(QuestionsMulti[questionCont])
+                    .then(res1 => {
+                        let propertyName  = Object.keys(res1); //get the name of the object property
+                        console.log('respuesta del res' , res1);
+                        if (String(res1[propertyName]) == 'n'){
+                            addBool = false;
+                            return objGeneral;
+                        }
+                        else{
+                            objGeneral[propertyName].push(String(res1[propertyName])); //concat res1 value to both object array
+                            addBool = true;
+                            return objGeneral;
+                        }
+                    });
+            }
+            console.log('sali del primer while', questionCont);
+            addBool = true;
+            questionCont++;
+        }
+        return objGeneral;
+    }
+
+    //Call the aysnconous function to run every thing sequentally
+    return callInquirers() 
+    .then(res => {
+        return res;
+    })   
+}
 
 // Function call to initialize app
-init();
+init()
+    .then((res)=> {
+        console.log(res)
+        let readmeObj = {
+            name: "auto-README.md"
+            ,content: ''
+        }
+        readmeObj.content = generateMarkdown(res);
+        return readmeObj
+    })
+    .then((readMe)=>{
+        console.log('final', readMe);
+        writeToFile(readMe.name, readMe.content);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+;
--- docs/utils/generateMarkdown.js
+++ docs/utils/generateMarkdown.js
@@ -1,19 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "no license") {
+    return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
+  } 
+  else {
+    return " ";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "no license") {
+    return `[${license}](https://choosealicense.com/licenses/${license})`;
+    } else {
+      return " ";
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "no license") {
+    return ` 
+  ## [License](#table-of-contents)
+  Application license: ${renderLicenseLink(license)}}
+    `;
+  } 
+  else {
+    return " ";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ## ${data.ProjectTitle}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Contact](#contact)
+  
+  ${renderLicenseSection(data.license)}
+
+  ## [Description](#table-of-contents)
+    ${data.ProjectDescription}
+  
+  ## [Installation](#table-of-contents)
+    ${data.ProjectInstall.map(step => `* ${step}`).join('\n    ')}
+
+  ## [Usage](#table-of-contents)
+    ${data.ProjectRun.map(step => `* ${step}`).join('\n    ')}
+
+  ## [Contributing](#table-of-contents)
+    ${data.ProjectGuidlines.map(step => `* ${step}`).join('\n    ')}
+
+  ## [Tests](#table-of-contents)
+    ${data.TestRun.map(step => `* ${step}`).join('\n    ')}
+  
+  ## [Contact](#table-of-contents)
+    - [GitHub: ${data.userGithub}](https://github.com/${data.userGithub})
+    - [Email: ${data.userEmail}](mailto:${data.userEmail})
 `;
 }
 
